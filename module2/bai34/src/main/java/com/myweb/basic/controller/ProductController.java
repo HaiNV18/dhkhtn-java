22f4221e621f5674263331ada51b629f6d212f2b
KINGDOM NVHAI <kingdomnvhai@gmail.com>
2026-02-17 19:31:30 +0700

    Create vidu bai5

--- module2/bai34/src/main/java/com/myweb/basic/controller/ProductController.java
+++ module2/bai34/src/main/java/com/myweb/basic/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.myweb.basic.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class ProductController {
 
+    @GetMapping("/demo/unit1_1")
+    public String unit1_1() {
+        return "unit1_1";
+    }
 }
---
+++ module2/bai5/vidu/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,80 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class DemoController {
+
+    @DeleteMapping("/orders/detail/body")
+    public ResponseEntity deleteOrdersFromBody(
+            @RequestBody Object ids
+    ){
+        System.out.println("Body data: " + ids.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/order/detail")
+    public ResponseEntity deleteOrder(
+            @RequestParam String id
+    ){
+        System.out.println("Id value: " + id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PutMapping("/category/detail/body")
+    public ResponseEntity updateCategoryFromBody(
+            @RequestBody Object productDetail
+    ){
+        System.out.println("Body data: " + productDetail.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/category/detail")
+    public ResponseEntity updateCategory(
+            @RequestParam String name,
+            @RequestParam(required = false) String price,
+            @RequestParam(defaultValue = "red") String color
+    ){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/product/detail")
+    public ResponseEntity createNewProduct(
+            @RequestParam String name,
+            @RequestParam(required = false) String price,
+            @RequestParam(defaultValue = "yellow") String color
+    ){
+        return new ResponseEntity<>(name, HttpStatus.OK);
+    }
+
+    @PostMapping("/product/detail/body")
+    public ResponseEntity createNewProductFromBodyParam(
+            @RequestBody Object productDetail
+    ){
+        System.out.println("Body data: " + productDetail.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+//    @GetMapping("/product/detail")
+//    public ResponseEntity getProductById(@RequestParam String id){
+//        return new ResponseEntity<>(id, HttpStatus.OK);
+//    }
+
+    @GetMapping("/products")
+    public ResponseEntity getAllProducts(){
+        List<String> productNames = new ArrayList<>();
+        productNames.add("Samsung");
+        return new ResponseEntity<>(productNames, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}/detail")
+    public ResponseEntity<String> getProductByIdFromPath(@PathVariable String id){
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+}
+
