816bc9435869fb60939bd2dfadf08bfbfe9e323c
KINGDOM NVHAI <kingdomnvhai@gmail.com>
2026-02-20 00:26:39 +0700

    Move vidu, create vidu bai7

---
+++ module2/vidu/src/main/java/com/example/demo/controller/UploadController.java
@@ -0,0 +1,72 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.ExternalApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import tools.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class UploadController {
+
+    @Autowired
+    ExternalApiService externalApiService;
+
+    private final TemplateEngine templateEngine;
+
+    public UploadController(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @GetMapping(value = "/product/detail_page", produces = MediaType.TEXT_HTML_VALUE)
+    public String showProductDetails() {
+        Context context = new Context();
+        context.setVariable("productName", "Marshall speaker 111");
+        return templateEngine.process("product_management/product_detail", context);
+    }
+
+    @GetMapping("/product/list")
+    public ResponseEntity<JsonNode> getProductList() {
+        try {
+            JsonNode data = externalApiService.fetchDataFromExternalApi("https://dummyjson.com/products");
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (Exception e){
+            JsonNode JsonNode = null;
+            return new ResponseEntity<>(JsonNode, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/product/upload_image")
+    public ResponseEntity<String> uploadProductImage(@RequestParam("file") MultipartFile file) {
+        final String UPLOAD_DIR = "src/main/resources/static/uploads/";
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("Missing file", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            Path uploadPath = Paths.get(UPLOAD_DIR);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            Path filePath = uploadPath.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), filePath);
+            return new ResponseEntity<>(filePath.toUri().toString(), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
