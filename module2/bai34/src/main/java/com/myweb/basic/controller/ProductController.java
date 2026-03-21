50682bc99fec621b201eeaf653c15165ed575c43
HaiNV18 <42117378+HaiNV18@users.noreply.github.com>
2026-03-21 12:31:31 +0700

    Create unit1_2.html

--- module2/bai34/src/main/java/com/myweb/basic/controller/ProductController.java
+++ module2/bai34/src/main/java/com/myweb/basic/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.myweb.basic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,17 @@ public class ProductController {
     @GetMapping("/demo/unit1_1")
     public String unit1_1() {
         return "unit1_1";
+    }
+
+    @GetMapping("/demo/unit1_2")
+    public String unit1_2(Model model) {
+        int a = 3;
+        int b = 4;
+
+        model.addAttribute("a", a);
+        model.addAttribute("b", b);
+        model.addAttribute("sum", a + b);
+        model.addAttribute("sub", a - b);
+        return "unit1_2";
     }
 }
